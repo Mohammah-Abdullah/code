1c4ae576ab48cd623f3cb28ec1691d622493bd28
Mohammah-Abdullah <abdullah03010666624@gmail.com>
2023-10-29 23:28:34 -0700

    Code Assignment

---
+++ Q13/Q13/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Write a C# programs to demonstrate the concepts of multilevel Inheritance
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication8
+{
+    class a
+    {
+        public void display()
+        {
+            Console.WriteLine("hahahaha");
+        }
+    }
+    class b : a //b is child of a
+    {
+        public void display1()
+        {
+            Console.WriteLine("hihihih");
+        }
+    }
+    class d : b //d is child of b
+    {
+        public void display2()
+        {
+            Console.WriteLine("hohohohoh");
+        }
+    }
+    class c
+    {
+        public static void Main()
+        {
+            d x = new d();//Normally object of child
+            x.display();
+            x.display1();
+            x.display2();
+        }
+    }
+}
---
+++ Q9/Q9/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Write a C# programs to demonstrate the concepts of single Inheritance
+
+using System;
+namespace InheritanceApplication
+{
+    class a
+    {
+        public void display()
+        {
+            System.Console.WriteLine("hahahaha");
+        }
+    }
+    class b : a //b is child of a
+    {
+        public void display1()
+        {
+            System.Console.WriteLine("hihihih");
+        }
+    }
+    class c
+    {
+        public static void Main()
+        {
+            b x = new b();
+            x.display();
+            x.display1();
+        }
+    }
+}
